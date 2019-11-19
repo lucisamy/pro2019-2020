5a6a1b5e1083a4522ee9bbdbc12cbe4d0714ac38
Lucía <luciasara.mercado@ikasle.egibide.org>
2019-11-19 20:04:16 +0100

    t3p1e21,t3p1e22 completados
    algunos comentarios añadidos.
    completado ej t3p1e21 (quizás es posible hacerlo sin variables globales y con funciones)
    completado ej t3p1e22

--- Tema3/Practica1/Pro3p1e21/src/pro3p1e21/Pro3p1e21.java
+++ Tema3/Practica1/Pro3p1e21/src/pro3p1e21/Pro3p1e21.java
@@ -4,6 +4,9 @@
  */
 package pro3p1e21;
 
+import javax.swing.JOptionPane;
+import Excepciones.*;
+
 /**
  *
  * @author Lucía
@@ -13,8 +16,48 @@ public class Pro3p1e21 {
     /**
      * @param args the command line arguments
      */
+    static boolean error;
+    static int n;
     public static void main(String[] args) {
-        // TODO code application logic here
+        //error=true;
+        do{
+            try{
+                pedirNumero();
+            }
+            catch(Exception e){
+                JOptionPane.showMessageDialog(null, "Problemas \n" + e.getClass() + " " + e.getMessage());
+            }
+        }
+        while(error);
+        //comprobar si es primo
+        comprobarPrimo();
     }
-    
+    public static void pedirNumero()throws Exception{
+        try{
+            n = Integer.parseInt(JOptionPane.showInputDialog("Teclea un número"));
+            if(n<=0)
+                throw new ValorNoValidoException();
+            error=false;
+        }
+        catch(NumberFormatException e){
+            JOptionPane.showMessageDialog(null, "hay que escribir un número");
+            error=true;
+        }
+        catch(ValorNoValidoException e){
+            JOptionPane.showMessageDialog(null, "El número debe ser mayor a 0");
+            error=true;
+        }
+    }
+    public static void comprobarPrimo(){
+        //un numero es primo solo si al dividirlo sucesivamente por 1-numero solo tiene resto 0 2 veces: al dividir por 1 y al dividir por si mismo.
+        int restoCero=0;
+        for(int divisor=1;divisor<=n;divisor++){
+            if(n%divisor==0)
+                restoCero++;
+        }
+        if(restoCero>2)
+            JOptionPane.showMessageDialog(null, "este número no es primo");
+        else
+            JOptionPane.showMessageDialog(null, "este número es primo");
+    }
 }
--- Tema3/Practica1/Pro3p1e22/src/pro3p1e22/Pro3p1e22.java
+++ Tema3/Practica1/Pro3p1e22/src/pro3p1e22/Pro3p1e22.java
@@ -4,6 +4,9 @@
  */
 package pro3p1e22;
 
+import javax.swing.JOptionPane;
+import Excepciones.*;
+
 /**
  *
  * @author Lucía
@@ -13,8 +16,59 @@ public class Pro3p1e22 {
     /**
      * @param args the command line arguments
      */
+    static boolean error;
     public static void main(String[] args) {
-        // TODO code application logic here
+        int n=0;
+        do{
+            try{
+                n = datoEntrada();
+            }
+            catch(Exception e){
+                JOptionPane.showMessageDialog(null, "Problemas \n" + e.getClass() + " " + e.getMessage());
+                System.exit(0);
+            }
+        }
+        while(error);
+        calcularPrimos(n);
+    }
+    public static int datoEntrada()throws Exception{
+        int n=0;
+        try{
+            n= Integer.parseInt(JOptionPane.showInputDialog("Teclea un número"));
+            if(n<=0)
+                throw new ValorNoValidoException();
+            error=false;
+        }
+        catch(ValorNoValidoException e){
+            JOptionPane.showMessageDialog(null, "debes escribir un número entero mayor a 0");
+            error=true;
+        }
+        catch(NumberFormatException e){
+            JOptionPane.showMessageDialog(null, "Escribe un número");
+            error=true;
+        }
+        return n;
+    }
+    public static void calcularPrimos(int n){
+        if(n==1)
+            JOptionPane.showMessageDialog(null, n + " es un número primo");
+        else
+        {
+            String numPrimos="Entre 1 y " + n + ": ";
+            for(int x =1;x<=n;x++){
+                //cada vez que cambiamos de número ponemos a 0 el contador de restoCero. 
+                int restoCero=0;
+                //miraremos si un número es primo o no
+                for(int divisor=1;divisor<=x;divisor++){
+                    if(x%divisor==0){
+                        restoCero++;
+                    }
+                }
+                if(restoCero<=2)
+                   numPrimos+= x + " ";
+            }
+            JOptionPane.showMessageDialog(null, numPrimos + "son números primos.");
+        }
     }
     
 }
